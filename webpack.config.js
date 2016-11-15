8eee102515708c4378af2ee6bfabf3f0c595dc36
DESKTOP-F5EVBDP\Pensarfeo <dryto25@hotmail.com>
2016-11-15 23:30:29 +0100

    added 9.3 bundles

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, "public"),
-        filename: 'bundle.js',
+        filename: '9.3.bundle.js',
         publicPath: ''
     },
     module: {
